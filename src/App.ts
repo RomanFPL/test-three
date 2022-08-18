6c82f3127486de21d51e9261f295d036cdc768c4
Roman Franko <roman.franko@voypost.com>
2022-08-18 20:08:11 +0300

    add: block

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,50 @@
 import * as THREE from "three";
 
+const button = document.querySelector("#cretion-button");
+const geometrySelector =
+  document.querySelector<HTMLInputElement>("#selected-geometry");
+const geometrySizeSelector =
+  document.querySelector<HTMLInputElement>("#selected-size");
+
+button?.addEventListener("click", () => {
+  if (!geometrySelector || !geometrySizeSelector) return;
+  const size = +geometrySizeSelector.value || 1;
+  let geometry;
+
+  if (geometrySelector.value === "cube") {
+    geometry = new THREE.BoxGeometry();
+  }
+
+  if (geometrySelector.value === "sphere") {
+    geometry = new THREE.SphereGeometry();
+  }
+
+  if (geometrySelector.value === "piramide") {
+    geometry = new THREE.CylinderGeometry(0, undefined, undefined, 4);
+  }
+
+  const object = new THREE.Mesh(geometry);
+  scene.add(object);
+
+  object.scale.set(size, size, size);
+
+  // TODO
+  object.position.set(0, 0, 0);
+
+  camera.position.z = 5;
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    object.rotation.x += 0.01;
+    object.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  }
+
+  animate();
+});
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -11,21 +56,3 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-}
-
-animate();
