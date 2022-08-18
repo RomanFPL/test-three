7f2cc1d89c9d4446d3e462609d77721d8786ad2a
Roman Franko <roman.franko@voypost.com>
2022-08-18 19:04:57 +0300

    add: functional style

--- src/App.ts
+++ src/App.ts
@@ -1,58 +1,31 @@
-import THREE, { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, TextureLoader, WebGLRenderer } from "three";
+import * as THREE from "three";
 
-export class App
-{
-    private camera: PerspectiveCamera;
-    private scene: Scene;
-    private mesh: Mesh;
-    private renderer: WebGLRenderer;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-    /**
-     * Based off the three.js docs: https://threejs.org/examples/?q=cube#webgl_geometry_cube
-     */
-    constructor()
-    {
-        this.camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-        this.camera.position.z = 400;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-        this.scene = new Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-        const texture = new TextureLoader().load("images/textures/crate.gif");
-        const geometry = new BoxGeometry(20, 20, 20);
-        const material = new MeshBasicMaterial({ map: texture });
+camera.position.z = 5;
 
-        this.mesh = new Mesh(geometry, material);
-        this.scene.add(this.mesh);
+function animate() {
+  requestAnimationFrame(animate);
 
-        this.renderer = new WebGLRenderer({ antialias: true, alpha: true });
-        this.renderer.setClearColor(0x000000, 0); // the default
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
-        document.body.appendChild(this.renderer.domElement);
-
-        window.addEventListener("resize", this.onWindowResize.bind(this), false);
-
-        this.animate();
-    }
-
-    private onWindowResize(): void
-    {
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-    }
-
-    private animate(): void
-    {
-        requestAnimationFrame(this.animate.bind(this));
-
-        this.mesh.rotation.x += 0.005;
-        this.mesh.rotation.y += 0.01;
-
-        this.renderer.render(this.scene, this.camera);
-    }
+  renderer.render(scene, camera);
 }
 
-new App();
+animate();
